d182694d0973fd0cb0185df1e4ecb50332e42ba6
miraks <a@vldkn.net>
2015-06-09 09:12:55 +0300

    fix and refactor hw1 6

--- hw1/6.hs
+++ hw1/6.hs
@@ -7,11 +7,11 @@ type Move = (Peg, Peg)
 hanoi3 :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi3 1 a b c = [(a, b)]
 hanoi3 n a b c =
-  stepOneMoves ++ stepTwoMoves ++ stepThreeMoves
+  step1 ++ step2 ++ step3
   where
-    stepOneMoves = hanoi3 (n-1) a c b
-    stepTwoMoves = hanoi3 1 a b c
-    stepThreeMoves = hanoi3 (n-1) c b a
+    step1 = hanoi3 (n-1) a c b
+    step2 = hanoi3 1 a b c
+    step3 = hanoi3 (n-1) c b a
 
 hanoi :: Integer -> [Peg] -> [Move]
 hanoi 1 (s:d:_) = [(s, d)]
@@ -19,9 +19,9 @@ hanoi k [s, d, t] = hanoi3 k s d t
 hanoi n ps =
   minimumBy (compare `on` length) steps
   where
-    steps = map (\k -> hanoi' k ps) [1..(n-1)]
+    steps = map (`hanoi'` ps) [1..(n-1)]
     hanoi' k (s:d:t:ps) =
-      step1 ++ step2 ++ step1
+      step1 ++ step2 ++ step3
       where
         step1 = hanoi k (s:d:t:ps)
         step2 = hanoi (n-k) (s:d:ps)
